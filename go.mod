9c75bc62baa08d1b39008c6112b8d5ba34cec50e
sukitha <sukithaj@gmail.com>
2021-12-02 08:33:16 +0530

    v1 with basic upload

--- go.mod
+++ go.mod
@@ -1,3 +1,24 @@
 module github.com/fs_agent
 
 go 1.17
+
+require (
+	github.com/0x19/goesl v0.0.0-20191107044804-3efcc2f41ccb
+	github.com/aws/aws-sdk-go v1.42.16
+	github.com/aws/aws-sdk-go-v2/config v1.10.3
+	github.com/aws/aws-sdk-go-v2/service/sqs v1.13.0
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.11.1 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.6.3 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.8.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.0.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.5.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.6.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.11.0 // indirect
+	github.com/aws/smithy-go v1.9.0 // indirect
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,178 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"runtime"
+	"strings"
 
-func main(){
-	fmt.Println("FS Agent")
+	"github.com/0x19/goesl"
+
+	//"github.com/aws/aws-sdk-go-v2/aws"
+	"encoding/json"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"github.com/aws/aws-sdk-go/aws"
+)
+
+var (
+	fshost   = flag.String("fshost", "localhost", "Freeswitch hostname. Default: localhost")
+	fsport   = flag.Uint("fsport", 8021, "Freeswitch port. Default: 8021")
+	password = flag.String("pass", "ClueCon", "Freeswitch password. Default: ClueCon")
+	timeout  = flag.Int("timeout", 10, "Freeswitch conneciton timeout in seconds. Default: 10")
+	region = flag.String("region", "", "Freeswitch region. Default: empty")
+)
+
+type SQSSendMessageAPI interface {
+	GetQueueUrl(ctx context.Context,
+		params *sqs.GetQueueUrlInput,
+		optFns ...func(*sqs.Options)) (*sqs.GetQueueUrlOutput, error)
+
+	SendMessage(ctx context.Context,
+		params *sqs.SendMessageInput,
+		optFns ...func(*sqs.Options)) (*sqs.SendMessageOutput, error)
+}
+
+func GetQueueURL(c context.Context, api SQSSendMessageAPI, input *sqs.GetQueueUrlInput) (*sqs.GetQueueUrlOutput, error) {
+	return api.GetQueueUrl(c, input)
+}
+
+func SendMsg(c context.Context, api SQSSendMessageAPI, input *sqs.SendMessageInput) (*sqs.SendMessageOutput, error) {
+	return api.SendMessage(c, input)
+}
+
+func main() {
+
+	systemEvents := map[string]bool{"RE_SCHEDULE": true, "HEARTBEAT": true} //Should handled in the agent side
+
+	queue := flag.String("q", "fs_events", "The name of the queue")
+	flag.Parse()
+
+	if *queue == "" {
+		fmt.Println("You must supply the name of a queue (-q QUEUE)")
+		return
+	}
+
+	
+	// this is the region based config loader for testing
+	// cfg, err := config.LoadDefaultConfig(
+	// 	context.TODO(),
+	// 	config.WithRegion("us-east-1"),
+	// )
+
+	cfg, conErr := config.LoadDefaultConfig(
+		context.TODO(),
+	)
+
+	if len(*region) > 0 {
+		cfg, conErr = config.LoadDefaultConfig(
+		context.TODO(),
+		config.WithRegion("us-east-1"),)
+	}
+
+	fmt.Println(cfg)
+	if conErr != nil {
+		panic("configuration error, " + conErr.Error())
+	}
+
+	sqs_client := sqs.NewFromConfig(cfg)
+	gQInput := &sqs.GetQueueUrlInput{
+		QueueName: queue,
+	}
+
+	result, err := GetQueueURL(context.TODO(), sqs_client, gQInput)
+	if err != nil {
+		fmt.Println("Got an error getting the queue URL:")
+		fmt.Println(err)
+		return
+	}
+
+	queueURL := result.QueueUrl
+
+	// Boost it as much as it can go ...
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	fs_client, err := goesl.NewClient(*fshost, *fsport, *password, *timeout)
+
+	if err != nil {
+		goesl.Error("Error while creating new client: %s", err)
+		return
+	}
+
+	goesl.Debug("Yuhu! New client: %q", fs_client)
+
+	// Apparently all is good... Let us now handle connection :)
+	// We don't want this to be inside of new connection as who knows where it my lead us.
+	// Remember that this is crutial part in handling incoming messages :)
+	go fs_client.Handle()
+
+	fs_client.Send("events json ALL")
+
+	//client.BgApi(fmt.Sprintf("originate %s %s", "sofia/internal/1001@127.0.0.1", "&socket(192.168.1.2:8084 async full)"))
+
+	for {
+		msg, err := fs_client.ReadMessage()
+
+		if err != nil {
+
+			// If it contains EOF, we really dont care...
+			if !strings.Contains(err.Error(), "EOF") && err.Error() != "unexpected end of JSON input" {
+				goesl.Error("Error while reading Freeswitch message: %s", err)
+			}
+			break
+		}
+
+		data, _ := json.Marshal(msg.Headers)
+
+		goesl.Notice("%s", msg.Headers["Event-Name"])
+		evName, evNameFound := msg.Headers["Event-Name"]
+		evTime, evTimeFound := msg.Headers["Event-Date-Timestamp"]
+		swName, swNameFound := msg.Headers["FreeSWITCH-Switchname"]
+
+		isSystem, isSystemFound := systemEvents[evName]
+
+		if isSystemFound && isSystem{
+			fmt.Printf("The event %s is a system event -> Not sending to sqs", evName)
+			continue
+		}
+
+		if  evNameFound &&  evTimeFound && swNameFound{ 
+			go func(){
+				sMInput := &sqs.SendMessageInput{
+					DelaySeconds: 10,
+					MessageAttributes: map[string]types.MessageAttributeValue{
+						"EventName": {
+							DataType:    aws.String("String"),
+							StringValue: aws.String(evName),
+						},
+						"EventDateTimestamp": {
+							DataType:    aws.String("String"),
+							StringValue: aws.String(evTime),
+						},
+						"Switchname": {
+								DataType:    aws.String("String"),
+								StringValue: aws.String(swName),
+						},
+					},
+					MessageBody: aws.String(string(data)),
+					QueueUrl:    queueURL,
+				}
+
+				resp, err := SendMsg(context.TODO(), sqs_client, sMInput)
+				if err != nil {
+					fmt.Println("Got an error sending the message:")
+					fmt.Println(err)
+					return
+				}
+
+				fmt.Println("Sent message with ID: " + *resp.MessageId)
+			}()
+		}else{
+			fmt.Println("No enough data found to process the event ... ", evName)
+		}
+
+	}
 }
